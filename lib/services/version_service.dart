50c6f98a16c579e0b87d832cf928e714efb515ef
hensybex <kamenetskiyto@gmail.com>
2024-01-18 20:23:08 +0300

    WTF

--- lib/services/version_service.dart
+++ lib/services/version_service.dart
@@ -3,17 +3,25 @@ import 'package:http/http.dart' as http;
 // Import the Letter model
 
 class VersionService {
-  static const String _baseUrl = 'https://pasternak-gpt.duckdns.org/api';
+  static const String _baseUrl = 'http://localhost:8080/api';
+  //static const String _baseUrl = 'https://pasternak-gpt.duckdns.org/api';
 
   Future<String> fetchServerVersion() async {
-    final uri = Uri.parse('$_baseUrl/get-version');
-    final response = await http.get(uri);
+    try {
+      final uri = Uri.parse('$_baseUrl/get-version');
+      final response = await http.get(uri);
 
-    if (response.statusCode == 200) {
-      final data = json.decode(response.body);
-      return data['version'];
-    } else {
-      throw Exception('Failed to load version from server');
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        return data['version'];
+      } else {
+        print(response.body);
+        throw Exception('Failed to load version from server');
+      }
+    } catch (e) {
+      print("IM HERE");
+      print(e);
+      return '0';
     }
   }
 }
